da4896ba329ff2f1208022632cad2ac94d2f103b
KiruthikaVenkatesh <33819810+KiruthikaVenkatesh@users.noreply.github.com>
2021-05-08 16:13:03 +0800

    Update Program.cs

--- PlanoApp/Program.cs
+++ PlanoApp/Program.cs
@@ -6,7 +6,7 @@ namespace PlanoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi, Hello World!");
         }
     }
 }
